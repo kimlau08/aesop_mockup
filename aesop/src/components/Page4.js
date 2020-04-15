265c84b04acee099612c22bc70983edea7d84ebb
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-15 18:47:30 -0500

    left paragraph

--- aesop/src/components/Page4.js
+++ aesop/src/components/Page4.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default function Page4 () {
-    return (
-        <React.Fragment>
-            <h2 style={{color:'salmon'}}>Welcome to the Page4</h2>
-            <h1>Test Test Test</h1>
-        </React.Fragment>
-    )
+export default class Page4 extends Component {
+    render() {
+        return (
+            <div className="pageFourContainer">
+                <div className="pageFourLeftText">
+                    <h2>Nurturing formulations</h2>
+                    <h3>For all ages, genders, and skin types</h3>
+                </div>
+                <div className="pageFourLeftParagraph">
+                    <p>Healthy skin is the product of careful cleansing, nourishing hydration and protection against environmental factors.</p>
+                </div>
+
+                
+                
+            </div>
+        )
+    }
 }
