c3f6fc05b9405ece6023338bdc29701be2d9bcdd
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-15 15:23:00 -0500

    Bara test

--- aesop/src/components/Page4.js
+++ aesop/src/components/Page4.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Page4 () {
     return (
         <React.Fragment>
-            <h1 style={{color:'salmon'}}>Welcome to the Page4</h1>
+            <h2 style={{color:'salmon'}}>Welcome to the Page4</h2>
         </React.Fragment>
     )
 }
