db05f2de8f4e3e0c217f6d6f154613dbade04e5d
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-15 15:28:42 -0500

    bara test

--- aesop/src/components/Page4.js
+++ aesop/src/components/Page4.js
@@ -4,6 +4,7 @@ export default function Page4 () {
     return (
         <React.Fragment>
             <h2 style={{color:'salmon'}}>Welcome to the Page4</h2>
+            <h1>Test Test Test</h1>
         </React.Fragment>
     )
 }
