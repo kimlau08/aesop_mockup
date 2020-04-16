c3cdaf0e7e90f0305441a998e7f4b550b356dff8
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-16 16:40:09 -0400

    app.css

--- aesop/src/components/Page1.js
+++ aesop/src/components/Page1.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Page1 () {
-    return (
-        <React.Fragment>
-            <h1 style={{color:'red'}}>Welcome to the Page1</h1>
-        </React.Fragment>
-    )
+class Page1 extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <div className="blackbar"></div>
+        </div> );
+    }
 }
+ 
+export default Page1;
