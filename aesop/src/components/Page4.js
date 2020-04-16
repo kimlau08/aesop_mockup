11466792c5852224f6b887c973cd1d8375758473
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-15 19:50:28 -0500

    uploaded 1st img of carosel

--- aesop/src/components/Page4.js
+++ aesop/src/components/Page4.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import ParsleySerum from '/Users/student_m19/Desktop/Homework/Aesop_Ecom/aesop_mockup/aesop/src/assets/for_all_products/ParsleySerum.png'
 
 export default class Page4 extends Component {
+    
+    
     render() {
         return (
             <div className="pageFourContainer">
@@ -10,6 +13,12 @@ export default class Page4 extends Component {
                 </div>
                 <div className="pageFourLeftParagraph">
                     <p>Healthy skin is the product of careful cleansing, nourishing hydration and protection against environmental factors.</p>
+                </div>
+                <div className="pageFourCarousel">
+                    {<img className="pageFourImg" src={ParsleySerum} alt="parsely serum"/> }
+                    <p className="parsleyHeadingOne">Parsely Seed Anti-Oxidant Serum</p>
+                    <p className="parsleyHeadingOne">Perfect for use in humid climates</p>
+                  
                 </div>
 
                 
