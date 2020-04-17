c09a41621801c673c728b7f2346391e93056c19c
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-17 10:29:21 -0500

    added footer nav bar

--- aesop/src/components/Footer.js
+++ aesop/src/components/Footer.js
@@ -3,8 +3,33 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                <h1>Footer</h1>
+            <div className="footerNav">
+                <nav> 
+                    <ul> 
+                        <h3>Contact Us</h3>
+                        <li>Contact us</li>
+                        <li>FAQ</li>
+                        <li>Delivery and Returns</li>
+                        <li>Track your order</li>
+                        <li>Check gift card balances</li>
+                        <li>Terms and conditions</li>
+                        <li>Accessibility</li>
+                    </ul>
+                    <ul>
+                        <h3>About</h3>
+                        <li>Our story</li>
+                        <li>Careers</li>
+                        <li>Press</li>
+                        <li>Corporate gifts</li>
+                        <li>Facial appointments</li>
+                    </ul>
+                    <ul>
+                        <h3>Social</h3>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                        <li>LinkedIn</li>
+                    </ul>
+                </nav>
             </div>
         )
     }
