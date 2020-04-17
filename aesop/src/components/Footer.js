523b30c9a2ca4d28412c3599ccc41feedde61569
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-17 10:44:27 -0500

    added more sections

--- aesop/src/components/Footer.js
+++ aesop/src/components/Footer.js
@@ -3,33 +3,38 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div className="footerNav">
-                <nav> 
-                    <ul> 
-                        <h3>Contact Us</h3>
-                        <li>Contact us</li>
-                        <li>FAQ</li>
-                        <li>Delivery and Returns</li>
-                        <li>Track your order</li>
-                        <li>Check gift card balances</li>
-                        <li>Terms and conditions</li>
-                        <li>Accessibility</li>
-                    </ul>
-                    <ul>
-                        <h3>About</h3>
-                        <li>Our story</li>
-                        <li>Careers</li>
-                        <li>Press</li>
-                        <li>Corporate gifts</li>
-                        <li>Facial appointments</li>
-                    </ul>
-                    <ul>
-                        <h3>Social</h3>
-                        <li>Instagram</li>
-                        <li>Twitter</li>
-                        <li>LinkedIn</li>
-                    </ul>
-                </nav>
+            <div>
+                <div>
+                    <nav> 
+                        <ul> 
+                            <h3>Contact Us</h3>
+                            <li>Contact us</li>
+                            <li>FAQ</li>
+                            <li>Delivery and Returns</li>
+                            <li>Track your order</li>
+                            <li>Check gift card balances</li>
+                            <li>Terms and conditions</li>
+                            <li>Accessibility</li>
+                        </ul>
+                        <ul>
+                            <h3>About</h3>
+                            <li>Our story</li>
+                            <li>Careers</li>
+                            <li>Press</li>
+                            <li>Corporate gifts</li>
+                            <li>Facial appointments</li>
+                        </ul>
+                        <ul>
+                            <h3>Social</h3>
+                            <li>Instagram</li>
+                            <li>Twitter</li>
+                            <li>LinkedIn</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="border">
+                    <p>Aesop</p>
+                </div>
             </div>
         )
     }
